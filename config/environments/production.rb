e68466533c988a658f9acc3a84527bcca2025269
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2014-03-23 19:17:42 -0300

    Email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,15 +82,10 @@ Colchonet::Application.configure do
   config.action_mailer.default_url_options = {
     host: "localhost"
   }
-
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    port:           ENV['MAILGUN_SMTP_PORT'],
-    address:        ENV['MAILGUN_SMTP_SERVER'],
-    username:       ENV['MAILGUN_SMTP_LOGIN'],
-    password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    port:           ENV['MAILGUN_SMTP_PORT'],
-    domain:         'fierce-everglades-2744.herokuapp.com',
-    authentication: :plain
+    address: "localhost",
+    port: 1025
   }
 end
